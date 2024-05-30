676259e5ec142e43797990bb885b86023e9e9578
AnasKhalidTECH <anaskhalid938@gmail.com>
2024-05-31 02:16:46 +0500

    Learnt how react flow and structure works

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,10 @@
+function App() {
+  return (
+   <>
+   <h1>Chai Aur React | Muhammad Anas</h1>
+   <p>I have learnt how to create react app in two ways one way is to directly installing react and second is to install it using vite and also learnt how to edit code.</p>
+   </>
+  );
+}
+
+export default App;
